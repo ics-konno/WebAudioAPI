a6e46908b9d8b4242147bfd149e5133369fec930
Fumiya Konno <f.kon94820@gmail.com>
2020-03-25 19:29:46 +0900

    sweepの実装

---
+++ oscillator/index.js
@@ -0,0 +1,52 @@
+// window.addEventListener("load", init);
+let audioContext = new AudioContext();
+let wave = audioContext.createPeriodicWave(wavetable.real, wavetable.imag);
+
+let attackTime = 0.2;
+let releaseTime = 0.2;
+let sweepLength = 2
+
+window.addEventListener("load", () => {
+  const button = document.querySelector("#start");
+  button.addEventListener("click", () => {
+    playSweep();
+  });
+
+  const attackCotrol = document.querySelector("#attack");
+  attackCotrol.addEventListener(
+    "input",
+    () => {
+      attackTime = Number(attackCotrol.value);
+    },
+    false
+  );
+
+  const releaseCotrol = document.querySelector("#release");
+  releaseCotrol.addEventListener(
+    "input",
+    () => {
+      releaseTime = Number(releaseCotrol.value);
+    },
+    false
+  );
+});
+
+function playSweep() {
+  const osc = audioContext.createOscillator();
+  osc.setPeriodicWave(wave);
+  osc.frequency.value = 440;
+
+  let sweepEnv = audioContext.createGain()
+  sweepEnv.gain.cancelScheduledValues(audioContext.currentTime)
+  sweepEnv.gain.setValueAtTime(0, audioContext.currentTime)
+
+  // 現在時間+attackTimeまで1に近づく
+  sweepEnv.gain.linearRampToValueAtTime(1, audioContext.currentTime + attackTime)
+  // 終わり時間-releaseTimeまで0に近づく
+  sweepEnv.gain.linearRampToValueAtTime(0, audioContext.currentTime + sweepLength - releaseTime)
+
+  osc.connect(sweepEnv).connect(audioContext.destination);
+  console.log("hi")
+  osc.start();
+  osc.stop(audioContext.currentTime + sweepLength);
+}
